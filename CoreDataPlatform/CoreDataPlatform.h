a5b8807902c6cc8a0a1e2f6d91f0461358be69f5
Aman Chawla <aman.chawla96@gmail.com>
2022-04-24 13:40:17 +0530

    first commit

---
+++ CoreDataPlatform/CoreDataPlatform.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataPlatform.h
+//  CoreDataPlatform
+//
+//  Created by Aman Chawla on 24/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDataPlatform.
+FOUNDATION_EXPORT double CoreDataPlatformVersionNumber;
+
+//! Project version string for CoreDataPlatform.
+FOUNDATION_EXPORT const unsigned char CoreDataPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataPlatform/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+inhibit_all_warnings!
+
+target 'CleanArchitecture' do
+  use_frameworks!
+
+  pod 'RxSwift', '~> 6.5.0'
+  pod 'RxCocoa', '~> 6.5.0'
+  pod 'QueryKit'
+
+end
+
+target 'Domain' do
+  use_frameworks!
+
+  pod 'RxSwift', '~> 6.5.0'
+
+end
+
+target 'CoreDataPlatform' do
+  use_frameworks!
+
+  pod 'RxSwift', '~> 6.5.0'
+  pod 'QueryKit'
+
+end
